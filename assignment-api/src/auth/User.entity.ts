deefeb6b3eb94808527a14ee1b00cc3b09e8ade8
khalid7487 <khalid15-7487@diu.edu.bd>
2022-05-22 23:00:33 +0600

    role and user table design

---
+++ assignment-api/src/auth/User.entity.ts
@@ -0,0 +1,122 @@
+import {
+    BeforeInsert,
+    Column,
+    Entity,
+    Index,
+    JoinColumn,
+    JoinTable,
+    ManyToMany,
+    OneToMany,
+    OneToOne,
+    PrimaryGeneratedColumn
+} from "typeorm";
+import bcrypt from "bcrypt";
+import {classToPlain, Exclude} from "class-transformer";
+import {IsEmail, Length} from "class-validator";
+import {Role} from "./Role.entity";
+// import {Vehicle} from "../vehicle/Vehicle.entity";
+// import {Identity} from "./Identity.entity";
+import {CoreEntity} from "../commons/CoreEntity";
+
+@Entity()
+export class User extends CoreEntity {
+
+    constructor(user: Partial<User>) {
+        super()
+        Object.assign(this, user)
+    }
+
+    // @Exclude()
+    @PrimaryGeneratedColumn({type: 'bigint'})
+    id: bigint
+
+    @Column({default: 0})
+    status: number          // 1, 2, 3, 4
+
+    @Column()
+    @Length(3, 255, {message: 'First Name must be at least 3 characters long'})
+    firstname: string
+
+    @Column()
+    @Length(3, 255, {message: 'Last name must be at least 3 characters long'})
+    lastname: string
+
+    @Index({unique: true})
+    @Column({nullable: true})
+    @IsEmail()
+    email: string
+
+    @Exclude()
+    @Column()
+    @Length(4, 255, {message: 'Password must be at least 6 characters long'})
+    password: string
+
+    @Index({unique: true})
+    @Column()
+    @Length(11, 15, {message: 'Number must be 15 characters long'})
+    phone: string
+
+    @Column({nullable: true})
+    dateOfbirth: string
+
+    @Index({unique: true})
+    @Column({nullable: true})
+    username: string
+
+    @Column({nullable: true})
+    user_status: string
+
+    @Column({nullable: true})
+    address: string
+
+
+    @ManyToMany(() => Role, {
+        nullable: true,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+    })
+    @JoinTable()
+    roles: Role[]
+
+    @Column({type: "text", nullable: true})
+    profile_image: string
+
+
+
+    // @OneToMany(() => Vehicle, vehicle => vehicle.user, {
+    //     nullable: true, cascade: true,
+    //     onDelete: 'CASCADE',
+    //     onUpdate: 'CASCADE'
+    // })
+    // vehicles: Vehicle[];
+
+
+    // @OneToOne(() => Identity, {
+    //     nullable: true, cascade: true,
+    //     onDelete: 'CASCADE',
+    //     onUpdate: 'CASCADE'
+    // })
+    // @JoinColumn()
+    // identity: Identity;
+
+
+    @Column({nullable: true})
+    short_bio: string
+
+    @Column({nullable: true})
+    bio: string
+
+    @Column({type: "text"})
+    totp_base64_string: string;
+
+
+    @BeforeInsert()
+    async hashPassword() {
+        this.password = await bcrypt.hash(this.password, 6)
+    }
+
+    toJSON() {
+        return classToPlain(this)
+    }
+
+}
---
+++ assignment-api/src/index.ts
@@ -0,0 +1,74 @@
+import "reflect-metadata";
+import express, {Request, Response} from 'express'
+import morgan from 'morgan'
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+// import authRoutes from "./auth/Auth.controller"
+// import vehicle from "./vehicle/Vehicle.controller"
+// import news from "./news/News.controller";
+
+// import trim from './middleware/trim'
+import databaseProviders from "../dbConnection";
+
+const fileUpload = require('express-fileupload');
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT
+
+app.use(cors())
+app.use(express.json()) //Used to parse JSON bodies
+app.use(express.urlencoded({extended: true}))
+app.use(morgan('dev'))
+// app.use(trim)
+app.use(cookieParser())
+
+
+// enable files upload
+app.use(fileUpload({createParentPath: true}));
+app.use(express.static('public'))
+app.use('/uploads', express.static('uploads'))
+// const path = require('path')
+// app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+
+
+const routePrefix = '/api/v1';
+
+app.get('/', async (req: Request, res: Response) => {
+    return res.status(200).json({status: true, message: 'Online Tutor app api'})
+})
+
+// request timeout
+app.use(function (req, res, next) {
+    res.setTimeout(120000000);
+    next();
+});
+
+// app.use(`${routePrefix}/auth`, authRoutes)
+
+
+// app.use(`${routePrefix}/news`, news)
+// app.use(`${routePrefix}/vehicle`, vehicle)
+
+
+// app.get('/uploads', function(req, res){
+//     res.sendFile(__dirname + '/uploads');
+// });
+
+
+app.listen(PORT, async () => {
+    console.log(`Server running at http://localhost:${PORT}`)
+
+
+    let defaultConnection = databaseProviders(process.env.DB_TYPE)
+
+    defaultConnection.then(() =>
+        console.log('Database connected')
+    ).catch(err => console.log("Database Failed to connected", err));
+
+
+})
+
+export default app;
